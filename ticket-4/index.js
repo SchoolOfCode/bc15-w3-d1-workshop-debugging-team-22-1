8b960cf59bebfc409a3d61d0656a635b0d4f940f
Christener19 <christenerleigh7070@gmail.com>
2023-09-18 15:36:49 +0100

    ticket 4

--- ticket-4/index.js
+++ ticket-4/index.js
@@ -1,15 +1,22 @@
 function getGradeFromPoints(points) {
-  if ((points) => 90) {
+  if (points >= 90) {
     return "A";
-  } else if (Points >= 70) {
+  } else if (points >= 70) {
     return "B";
-  } else if (pointss >= 50) {
+  } else if (points >= 50) {
     return "C";
-  } else if (poInts >= 40) {
+  } else if (points >= 40) {
     return "D";
   }
   return "E";
 }
 
-const grade = getGradeFromPoints(90);
-console.log(Grade, "should be A");
+
+
+console.log(getGradeFromPoints(100))
+console.log(getGradeFromPoints(90))
+console.log(getGradeFromPoints(70))
+console.log(getGradeFromPoints(50))
+console.log(getGradeFromPoints(40))
+console.log(getGradeFromPoints(39))
+console.log(getGradeFromPoints(0))
