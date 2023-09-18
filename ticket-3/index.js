d0a0e031e818a05d41a02fb33d3cd88f5dcfd954
Christener19 <christenerleigh7070@gmail.com>
2023-09-18 15:01:22 +0100

    ticket 3

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,12 +1,16 @@
 function countNumberOfNinesInNumber(number) {
   let count = 0;
-
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
-    if ("9" === digit) {
+let numArray = Array.from(number.toString())
+  for (let i = 0; i < number.toString().length; i++) {
+    let digit = numArray[i];
+    if (9 == digit) {
       count++;
     }
   }
 
   return count;
 }
+console.log(countNumberOfNinesInNumber(99.999))
+console.log(countNumberOfNinesInNumber(9))
+console.log(countNumberOfNinesInNumber(989))
+console.log(countNumberOfNinesInNumber(1115.1257846))
